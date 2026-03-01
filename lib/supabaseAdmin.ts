f42e12339a2d3b909755c3d0aaa9bf57c1046b33
Ivan Hernandez Hernandez <ivanhh2003@gmail.com>
2026-02-28 20:24:24 -0600

    Correccion en el envio de correos y seguridad de administrador y usuarios

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -2,11 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-    },
-  }
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
