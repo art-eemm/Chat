ca5f4d6d15fa505f53c4a2967cc1ef990b4e80ce
Ivan Hernandez Hernandez <ivanhh2003@gmail.com>
2026-03-02 20:36:08 -0600

    Se corrigen nombre de variable de entorno

--- lib/verifyToken.ts
+++ lib/verifyToken.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export async function verifyToken(token: string) {
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
   const { data, error } = await supabase.auth.getUser(token);
