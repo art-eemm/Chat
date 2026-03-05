2bdf9289585a665fcc6ef6d7dad9e4cbbf0f5234
Ivan Hernandez Hernandez <ivanhh2003@gmail.com>
2026-03-05 17:35:18 -0600

    (FIX) Correccion de ortografia

--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -18,18 +18,14 @@ export async function GET(req: Request) {
       return NextResponse.json({ error: "Token inválido" }, { status: 401 });
     }
 
-    // Obtener todos menos el usuario autenticado
-    const { data: usuarios, error } = await supabaseAdmin
-      .from("perfiles")
-      .select("id_perfil, nombre_usuario, foto_url, en_linea, ultima_conexion")
-      .neq("id_perfil", user.id);
+    //* Obtener todos menos el usuario autenticado
+    const { data: usuarios, error } = await supabaseAdmin.from("perfiles").select("id_perfil, nombre_usuario, foto_url, en_linea, ultima_conexion").neq("id_perfil", user.id);
 
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
 
     return NextResponse.json({ usuarios }, { status: 200 });
-
   } catch (error) {
     return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
   }
