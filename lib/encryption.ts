595fbc3fffac0841e518091e4cca5026f29ce1c7
Ivan Hernandez Hernandez <ivanhh2003@gmail.com>
2026-03-05 17:33:57 -0600

    (FIX) Cambio en la encriptacion y desencriptacion del texto

--- lib/encryption.ts
+++ lib/encryption.ts
@@ -1,10 +1,25 @@
 import crypto from "crypto";
 
 const algorithm = "aes-256-cbc";
-const secretKey = Buffer.from(process.env.ENCRYPTION_KEY!, "hex");
+const secretKey = crypto.createHash("sha256").update(process.env.ENCRYPTION_KEY!).digest();
 
 //* Cifrado de texto
 export function encryptText(text: string) {
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+
+  const encrypted = Buffer.concat([
+    cipher.update(text, "utf8"),
+    cipher.final(),
+  ]);
+
+  return {
+    contenido_cifrado: encrypted.toString("hex"),
+    iv: iv.toString("hex"),
+  };
+}
+/* export function encryptText(text: string) {
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
   const encrypted = Buffer.concat([ cipher.update(text, "utf8"), cipher.final(), ]);
@@ -12,15 +27,27 @@ export function encryptText(text: string) {
     contenido_cifrado: encrypted.toString("hex"),
     iv: iv.toString("hex"),
   };
-}
+} */
 
 //*  Descifrado de texto
 export function decryptText(contenido_cifrado: string, ivHex: string) {
   const iv = Buffer.from(ivHex, "hex");
+
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+
+  const decrypted = Buffer.concat([
+    decipher.update(Buffer.from(contenido_cifrado, "hex")),
+    decipher.final(),
+  ]);
+
+  return decrypted.toString("utf8");
+}
+/* export function decryptText(contenido_cifrado: string, ivHex: string) {
+  const iv = Buffer.from(ivHex, "hex");
   const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
   const decrypted = Buffer.concat([ decipher.update(Buffer.from(contenido_cifrado, "hex")), decipher.final(), ]);
   return decrypted.toString("utf8");
-}
+} */
 
 //* Cifrado de audio, video o foto
 export function encryptFile(buffer: Buffer) {
